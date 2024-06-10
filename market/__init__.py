ce2ef088457f0dbe9fe23984a7e7873526fe0910
remy patrick <shimirwaremyklaus@gmail.com>
2024-06-10 20:56:05 +0200

    first commit

---
+++ market/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] ='c55e835ed9689094988ce39a'
+
+db = SQLAlchemy(app)
+app.app_context().push()
+bcryt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view= "login_page"
+login_manager.login_message_category = "info"
+from market import routes
